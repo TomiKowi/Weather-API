baab15a23b611403a79c6e20c375a9d43b28dfce
Tomek Kowol <tomek.pdp@gmail.com>
2019-07-31 02:51:44 +0200

    Updates

---
+++ precache-manifest.9d8b4227b50abc4f100c1e949580643f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636bddce444b244ed3d54f6873197d4d",
+    "url": "/Weather-API/index.html"
+  },
+  {
+    "revision": "d42fbc76207f8f0d9244",
+    "url": "/Weather-API/static/css/main.a66bfb55.chunk.css"
+  },
+  {
+    "revision": "32ba428a442bce32e14e",
+    "url": "/Weather-API/static/js/2.249420ef.chunk.js"
+  },
+  {
+    "revision": "d42fbc76207f8f0d9244",
+    "url": "/Weather-API/static/js/main.cfb26629.chunk.js"
+  },
+  {
+    "revision": "5215fa4a0930336c2c97",
+    "url": "/Weather-API/static/js/runtime~main.bb912a8e.js"
+  }
+]);
